88fc7385cf56c545195df1a5280604c66cac4746
Tanvi Mathur <31370075+tanvi-mathur@users.noreply.github.com>
2025-01-05 21:00:18 +0530

    Update SW1.py

--- SW1.py
+++ SW1.py
@@ -1,5 +1,9 @@
 import sys
+"""To run: 
+python SW1.py input.txt output.txt
+E.g. python SW1.py input1.txt output1.txt For SW1.py
 
+Input file .txt is in following format: gate x-coordinate y-coordinate"""
 gh={}
 gw={}
 file= str(sys.argv[1])
